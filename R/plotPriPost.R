0a45841dd94d085acffebd6323d7ff8065ba3236
Pascal PERNOT <pascal.pernot@u-psud.fr>
2018-12-11 10:33:30 +0100

    Added optional KL-divergence display to Prior vs. Posterior plots.

--- R/plotPriPost.R
+++ R/plotPriPost.R
@@ -5,20 +5,29 @@
 #' @param tag name of the variable
 #' @param xlim an interval
 #' @param gPars a list of graphical parameters and colors
-#' @return Produces a plot.
+#' @param kl a logical to compute and display KL-divergence
+#' @return Produces a plot. The KL divergence in bits is
+#'         displayed in the title if `kl == TRUE`.
 #' @author Pascal PERNOT
 #' @export
 
-plotPriPost      <- function(pri,pos,tag,xlim=range(c(pri,pos)), gPars) {
+plotPriPost      <- function(pri,pos,tag,xlim=range(c(pri,pos)),gPars,
+                             kl = TRUE) {
   # Extract graphical parameters
   for (n in names(gPars))
     assign(n,rlist::list.extract(gPars,n))
+  
+  # Compute KL-divergence in bits
+  if(kl)
+    kld = 1.443 * mean(FNN::KL.divergence(pos, pri))
   
   # Plot overlapped densities for 2 samples
   d = density(pri)
   d$y = d$y/max(d$y)
   plot(d, type = 'l', col = cols[4],
-       main = tag,
+       main = ifelse(kl,
+                     paste0(tag,' / KL: ',signif(kld,2)),
+                     tag),
        xlab = '', xlim = xlim,
        ylab = 'Norm. density', ylim = c(0,1.1), yaxs = 'i')
   polygon(d$x,d$y,rev(d$w),0*d$y,col=col_tr2[4],border=NA)
